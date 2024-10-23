42e10dbe9fa5be45d858e6857374dd98cdb8423f
cekwedike <c.ekwedike@alustudent.com>
2024-10-23 22:18:15 +0200

    Refactor main.dart and add navigation to explore screen and purchase history screen

---
+++ farm_fresh/lib/main.dart
@@ -0,0 +1,41 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'screens/explorescreen.dart';
+import 'screens/purchase_history_screen.dart';
+import 'screens/details.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Farm Fresh',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF1B8E3D),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Color(0xFF1B8E3D)),
+          titleTextStyle: TextStyle(
+            color: Color(0xFF1B8E3D),
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FarmFreshScreen(),
+        '/purchase_history': (context) => const PurchaseHistoryScreen(),
+        '/product_details': (context) => const ProductDetailScreen(),
+      },
+    );
+  }
+}
